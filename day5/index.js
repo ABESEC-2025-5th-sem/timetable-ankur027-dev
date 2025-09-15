546bd7494b7cf97cd92b47570cc7b66615f63eb1
ankur027-dev <tiwariankit121@yahoo.com>
2025-09-15 15:36:11 +0530

    day6

---
+++ day5/index.js
@@ -0,0 +1,2 @@
+console.log("Welcome to the FSD session!");
+console.log(5+10);
---
+++ day6/function.js
@@ -0,0 +1,12 @@
+// function fun(){
+//     console.log("hello");
+
+// }
+// console.log(fun);
+// console.log(fun());
+const a=function b(){
+    console.log("hello");
+}
+console.log(a);
+console.log(b);
+console.log(a(3));
