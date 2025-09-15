0da67f0bb1e25befadc80e158a0fc284fa2c1a8a
ankur027-dev <tiwariankit121@yahoo.com>
2025-09-15 15:36:11 +0530

    day6

---
+++ day6/index.js
@@ -0,0 +1,24 @@
+// let x=5;
+// function modify(val){
+//     val=10;
+// }
+// modify(x);
+// console.log(x);
+// let y=[2,3,4];
+// function modify(arr){
+//     y.push(5);
+// }
+// modify(y);
+// console.log(y);
+// (()=>{
+//     var x = y=6;
+
+
+// })();
+// // console.log(x);
+// console.log(y);     
+// console.log(typeof x);
+// console.log(typeof y);
+// let a=23 - "23"+true+false-true+23+(-23);
+// console.log(a);
+// console.log(typeof NULL);
