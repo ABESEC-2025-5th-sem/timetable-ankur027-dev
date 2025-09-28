b64a0767c91a7c8c8427e886fa9b50d363d304e9
ankur027-dev <tiwariankit121@yahoo.com>
2025-09-28 14:14:30 +0530

    hello

---
+++ day9/index.js
@@ -0,0 +1,60 @@
+// console.log("hello world")
+//  var a=12;
+//  console.log(a);
+
+// let a=40;
+// if(a>20){
+//     let a=90;
+//     console.log("a inside if"+a);
+
+// }
+// else{
+//     console.log("hii... inside else");
+// }
+//  console.log("a is outside "+a);
+
+
+// function greetings(msg){
+//     return  "hii....welcome to Abes ds"+msg;
+//     console.log("ENded");
+// }
+//  const data= function greetings(msg){
+//     return "hii...welcome to abes"+msg;}
+//  console.log("data"+msg);
+
+
+// const data=(myname)=>{
+//     return "hii"+myname+"welcome to fsd";
+
+// }
+
+// const msg=data("ankur");
+// console.log(msg);
+
+// const data =msg=>msg;
+// console.log("hiii....");
+
+// function msg(){
+//     console.log("welcome to abes");
+
+// }
+// // setTimeout(() => {
+// //     console.log("hey")
+// // },1000);
+// setInterval(msg,1000);
+
+function selectlanguage(lang){
+    let data;
+    if(lang=="java"){
+        function javacompiler(){
+            return "java compiler calling ";
+            data =javacompiler();
+        }
+        else if(lang=="c"){
+            function cCompiler(){
+                
+            }
+
+        }
+    }
+}
